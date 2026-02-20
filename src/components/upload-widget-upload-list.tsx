39be2a58585c2de64487530388cad2002d3aa2cf
Marco Aurélio S.S.Jr. <aureliojuniorcmrj@hotmail.com>
2026-02-20 19:52:18 -0300

    implement upload list items

--- src/components/upload-widget-upload-list.tsx
+++ src/components/upload-widget-upload-list.tsx
@@ -1,5 +1,15 @@
+import { UploadWidgetUploadItem } from "./upload-widget-upload-item";
+
 export function UploadWidgetUploadList() {
   return (
-    <div className="px-3 h-48"></div>
+    <div className="px-3 flex flex-col gap-3">
+      <span className="text-sm font-medium">
+        Uploaded files <span className="text-zinc-400">(2)</span>
+      </span>
+      <div className="flex flex-col gap-1">
+        <UploadWidgetUploadItem />
+        <UploadWidgetUploadItem />
+      </div>
+    </div>
   )
 }
